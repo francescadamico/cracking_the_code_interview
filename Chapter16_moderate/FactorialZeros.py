22f40980bda3f53d38ec9bc52eca5698ff7a0c08
francesca <missjulia83@gmail.com>
2021-01-10 14:27:09 +0100

    solved FactorialZeros

---
+++ Chapter16_moderate/FactorialZeros.py
@@ -0,0 +1,19 @@
+'''
+Write an algorithm which computes the number of trailing zeros in n factorial.
+'''
+import math 
+
+
+def factorial_zeros(n):
+
+  k = int(math.log(n, 5))
+  trail_zeros = 0
+
+  for i in range(1, k+1):
+    trail_zeros += n // 5**i
+
+  return trail_zeros
+
+
+num = 126
+print(factorial_zeros(num))
