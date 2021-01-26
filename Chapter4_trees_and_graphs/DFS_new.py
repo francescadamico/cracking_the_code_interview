8eb5a06b53e706ac6b2390495ba0dbf0298b05b2
francesca <missjulia83@gmail.com>
2021-01-26 10:23:46 +0100

    coplete version of DFS

--- Chapter4_trees_and_graphs/DFS_new.py
+++ Chapter4_trees_and_graphs/DFS_new.py
@@ -2,27 +2,38 @@
 class Node:
   def __init__(self, data):
     self.data = data
-    self.children = []
+    self.adj = []
 
 '''
 this function defines what has to be done with the node being visited: it is the one that has to be used to make the desired operation on the visited node
 '''
 def visit_node(node):
-  return node
+  return node.data
 
 
-def DFS(node, visited_nodes = []):
-  if node == None:
-    return
-  if node not in visited_nodes:
-    visited_nodes.append(visit_node(node))
-    for n in node.children:    
-      visited_nodes = DFS(n, visited_nodes)
-  return visited_nodes
+def DFS_visit(n, dfs, visited_nodes):
+    
+  for v in n.adj:
+    if v not in visited_nodes:  
+      visited_nodes.add(v)       
+      DFS_visit(v, dfs, visited_nodes) 
+      dfs += [visit_node(v)]
 
-
+def DFS(N):
+  
+  dfs = []
+  visited_nodes = set()
+  for n in N:
+    if n not in visited_nodes:
+      visited_nodes.add(n)
+      DFS_visit(n, dfs, visited_nodes)
+      dfs += [visit_node(n)]
+      
+  return dfs
 
 R = Node(1)
+
+T = Node(8)
 
 N1 = Node(2)
 N2 = Node(3)
@@ -30,14 +41,17 @@ N3 = Node(4)
 N4 = Node(5)
 N5 = Node(6)
 
-R.children = [N1] 
-N1.children = [N2, N4, N5]
-N2.children = [N3]
-N3.children = [N4]
+R.adj = [N1] 
+N1.adj = [N2, N4, N5]
+N2.adj = [N3]
 
-nodes_order = DFS(R)
-for i in nodes_order: 
-  print(i.data)
-  
+N4.adj = [N2, N3, T]
+
+T.adj = [N2]
+
+nodes = [R,T, N1, N2, N3, N4]
+nodes_order = DFS(nodes)
+
+print(nodes_order)
 
 
